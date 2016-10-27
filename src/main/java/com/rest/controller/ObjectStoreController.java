3e5a26098e1edd6f50c9dcec3876d7f315dadc59
Pushkar Umaranikar <pushkarumaranikar1@gmail.com>
2016-10-27 06:20:14 -0500

    Add request mapping in object store controllerWq

--- src/main/java/com/rest/controller/ObjectStoreController.java
+++ src/main/java/com/rest/controller/ObjectStoreController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 @RestController
+@RequestMapping(value="/objectStore")
 public class ObjectStoreController {
 	
 	
@@ -43,5 +44,7 @@ public class ObjectStoreController {
 		    
 		    return new ResponseEntity<>(HttpStatus.OK);
 		  } 
+	
+	
 
 }
